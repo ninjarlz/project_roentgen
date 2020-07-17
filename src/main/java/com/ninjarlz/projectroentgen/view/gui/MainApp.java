27763ad430bf566e68a7789622fe0a63e1c3d6a4
ninjarlz <michal.kusmidrowicz@gmail.com>
2020-07-17 13:28:15 +0200

    initial commit with project set-up

---
+++ src/main/java/com/ninjarlz/projectroentgen/view/gui/MainApp.java
@@ -0,0 +1,31 @@
+package com.ninjarlz.projectroentgen.view.gui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class MainApp extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        URL location = getClass().getResource("/com/ninjarlz/projectroentgen/gui/MainView.fxml");
+        FXMLLoader fxmlLoader = new FXMLLoader(location);
+        fxmlLoader.setResources(ResourceBundle.getBundle("i18n.SudokuBundle", new Locale("en", "EN")));
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root);
+        stage.setTitle("App");
+        stage.setScene(scene);
+        MainView controller = fxmlLoader.getController();
+        controller.setStage(stage);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/main/java/com/ninjarlz/projectroentgen/view/gui/MainView.java
@@ -0,0 +1,74 @@
+package com.ninjarlz.projectroentgen.view.gui;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.*;
+
+
+public class MainView implements Initializable {
+
+
+    public Menu fileMenu;
+    public MenuItem loadMenuItem;
+    public MenuItem closeMenuItem;
+    public BorderPane mainPane;
+    public Menu languageMenu;
+    public MenuItem englishMenuItem;
+    public MenuItem polishMenuItem;
+    public Label authorsLabel;
+    public AnchorPane anchorPane;
+    private FileChooser loadFileChooser = new FileChooser();
+    private Stage stage;
+    private Locale englishLocale = new Locale("en", "EN");
+    private ResourceBundle englishBundle = ResourceBundle.getBundle("i18n.SudokuBundle", englishLocale);
+    private ResourceBundle polishBundle = ResourceBundle.getBundle("i18n.SudokuBundle");
+    private ResourceBundle currentBundle = englishBundle;
+
+    private enum Language {ENGLISH, POLISH}
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+
+    public void closeAction(ActionEvent actionEvent) {
+        Platform.exit();
+    }
+
+
+    public void changeLangauge(Language language) {
+        switch (language) {
+            case ENGLISH:
+                currentBundle = englishBundle;
+                break;
+            case POLISH:
+                currentBundle = polishBundle;
+                break;
+        }
+        fileMenu.setText(currentBundle.getString("file"));
+        loadMenuItem.setText(currentBundle.getString("load"));
+        closeMenuItem.setText(currentBundle.getString("close"));
+        languageMenu.setText(currentBundle.getString("language"));
+        englishMenuItem.setText(currentBundle.getString("english"));
+        polishMenuItem.setText(currentBundle.getString("polish"));
+    }
+
+    public void changeToPolishAction(ActionEvent actionEvent) {
+        changeLangauge(Language.POLISH);
+    }
+
+    public void changeToEnglishAction(ActionEvent actionEvent) {
+        changeLangauge(Language.ENGLISH);
+    }
+}
